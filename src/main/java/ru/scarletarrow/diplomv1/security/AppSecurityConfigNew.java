cf806a5d338b453db12becad8666ad4b149bba26
Anton Yurkov <anton20001701@yandex.ru>
2022-07-18 13:02:46 +0300

    removed deprecated WebSecurityConfigurerAdapter

---
+++ src/main/java/ru/scarletarrow/diplomv1/security/AppSecurityConfigNew.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2022. Program made by Anton Yurkov.
+ */
+
+package ru.scarletarrow.diplomv1.security;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.scarletarrow.diplomv1.filter.CustomAuthenticationFilter;
+import ru.scarletarrow.diplomv1.filter.CustomAuthorizationFilter;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration
+public class AppSecurityConfigNew  {
+    public AppSecurityConfigNew(PasswordEncoder bCryptPasswordEncoder,
+                               UserDetailsService userDetailsService) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.userDetailsService = userDetailsService;
+    }
+
+    private final PasswordEncoder bCryptPasswordEncoder;
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+        var authenticationManager = builder.build();
+
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager);
+
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers("/login/**", "/api/token/refresh").permitAll();
+        http.authorizeRequests().antMatchers(GET,"/api/v1/**").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(POST,"/api/v1/**").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().antMatchers("/graphiql/**").permitAll();
+        http.authorizeRequests().antMatchers("/graphql/**").permitAll();
+        http.authorizeRequests().antMatchers("/voyager/**").permitAll();
+        http.authorizeRequests().antMatchers("/playground/**").permitAll();
+
+        http.authorizeRequests().anyRequest().permitAll();
+        http.authenticationManager(authenticationManager);
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+
+//    @Bean(name = "authenticationManager")
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
+}
